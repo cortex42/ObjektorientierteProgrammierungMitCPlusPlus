aabec3da769eff07a2932091617ef9e1597d840c
Ivo Palazzolo <cortex42@users.noreply.github.com>
2017-03-10 00:09:05 +0100

    finished blatt06

--- blatt06/TestTrie.cpp
+++ blatt06/TestTrie.cpp
@@ -1,18 +1,36 @@
 #include "Trie.hpp"
 
+void visit(const std::string &word) {
+    std::cout << word << std::endl;
+}
+
 int main() {
-	// default constructor
-	Trie<std::string> trie1;
-	
-	// copy constructor
-	Trie<std::string> trie2(trie1);
-	
-	// assignment
-	Trie<std::string> trie3;
-	trie3 = trie2;
-	
-	
-	// add something
-	std::string test_string("world");
-	trie1.add("hello", &test_string);
+    // default constructor
+    Trie<std::string> trie1;
+
+    // copy constructor
+    Trie<std::string> trie2(trie1);
+
+    // assignment
+    Trie<std::string> trie3;
+    trie3 = trie2;
+
+
+    // add something
+    std::string test_string_world("world");
+    std::string test_string_foo("foo");
+    trie1.add("hello", test_string_world);
+    std::cout << trie1.count() << std::endl;
+    trie1.add("hell", test_string_foo);
+    std::cout << trie1.count() << std::endl;
+
+    // visit
+    trie1.visit("hell", visit);
+
+    // replace
+    trie1.add("hell", std::string("bar"));
+    std::cout << trie1.count() << std::endl;
+    trie1.visit("hell", visit);
+
+    // todo check with valgrind
 }
