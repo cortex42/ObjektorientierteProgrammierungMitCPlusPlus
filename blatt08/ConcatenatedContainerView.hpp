5e49b9e0fd7b4528d3a9b70b4c6c1c6ffa15fa55
cortex42 <ivo.palazzolo@gmx.de>
2017-03-12 18:47:10 +0100

    blatt 08 finished

--- blatt08/ConcatenatedContainerView.hpp
+++ blatt08/ConcatenatedContainerView.hpp
@@ -6,20 +6,41 @@
 #define CONCATENATEDCONTAINERVIEW_HPP
 
 #include <iterator>
-#include <iostream>
 
 template<typename T, typename U>
 class ConcatenatedContainerView {
 public:
-    ConcatenatedContainerView(T container1, U container2)
+    ConcatenatedContainerView() {};
+
+    ConcatenatedContainerView(const T &container1, const U &container2)
             : begin_iterator{container1.begin(), container1.end(), container1.begin(), container2.begin(),
                              container2.end(), container2.begin()},
               end_iterator{container1.begin(), container1.end(), container1.end(), container2.begin(), container2.end(),
                            container2.end()} {}
 
+    ConcatenatedContainerView(const ConcatenatedContainerView &other) : begin_iterator{other.begin_iterator},
+                                                                        end_iterator{other.end_iterator} {};
+
+    ConcatenatedContainerView(ConcatenatedContainerView &&rhs) : ConcatenatedContainerView() {
+        swap(*this, rhs);
+    }
+
+    ConcatenatedContainerView &operator=(ConcatenatedContainerView rhs) {
+        swap(*this, rhs);
+        return *this;
+    }
+
+    friend void swap(ConcatenatedContainerView &view1, ConcatenatedContainerView &view2) {
+        std::swap(view1.begin_iterator, view2.begin_iterator);
+        std::swap(view1.end_iterator, view2.end_iterator);
+    }
+
     template<typename V, typename W>
     class ConcatenatedIterator {
     public:
+
+        ConcatenatedIterator() {};
+
         ConcatenatedIterator(V begin_container1, V end_container1, V position_container1, W begin_container2,
                              W end_container2, W position_container2) :
                 begin_container1{begin_container1}, end_container1{end_container1},
@@ -27,8 +48,53 @@ public:
                 begin_container2{begin_container2}, end_container2{end_container2},
                 position_container2{position_container2} {}
 
+        ConcatenatedIterator(const ConcatenatedIterator &other) : begin_container1{other.begin_container1},
+                                                                  begin_container2{other.begin_container2},
+                                                                  end_container1{other.end_container1},
+                                                                  end_container2{other.end_container2},
+                                                                  position_container1{other.position_container1},
+                                                                  position_container2{other.position_container2} {}
+
+        ConcatenatedIterator(ConcatenatedIterator &&rhs) : ConcatenatedIterator() {
+            swap(*this, rhs);
+        }
+
+        friend void swap(ConcatenatedIterator &iterator1, ConcatenatedIterator &iterator2) {
+            std::swap(iterator1.begin_container1, iterator2.begin_container1);
+            std::swap(iterator1.end_container1, iterator2.end_container1);
+            std::swap(iterator1.position_container1, iterator2.position_container1);
+            std::swap(iterator1.begin_container2, iterator2.begin_container2);
+            std::swap(iterator1.end_container2, iterator2.end_container2);
+            std::swap(iterator1.position_container2, iterator2.position_container2);
+        }
+
+        ConcatenatedIterator &operator=(ConcatenatedIterator &&rhs) {
+            swap(*this, rhs);
+            return *this;
+        }
+
+        bool operator==(const ConcatenatedIterator &other) const {
+            if (position_container1 == end_container1 && position_container2 == end_container2
+                && other.position_container1 == other.end_container1 &&
+                other.position_container2 == other.end_container2) {
+                return true;
+            }
+
+            if (position_container1 != end_container1 && other.position_container1 != other.end_container1) {
+                return position_container1 == other.position_container1;
+            }
+
+            if (position_container1 == end_container1 && position_container2 != end_container2
+                && other.position_container1 == other.end_container1 &&
+                other.position_container2 != other.end_container2) {
+                return position_container2 == other.position_container2;
+            }
+
+            return false;
+        }
+
         bool operator!=(const ConcatenatedIterator &other) const {
-            // todo
+            return !(*this == other);
         }
 
         ConcatenatedIterator &operator++() {
@@ -57,16 +123,16 @@ public:
         W begin_container2, end_container2, position_container2;
     };
 
-    ConcatenatedIterator<typename T::iterator, typename U::iterator> begin() const {
+    ConcatenatedIterator<typename T::const_iterator, typename U::const_iterator> begin() const {
         return begin_iterator;
     }
 
-    ConcatenatedIterator<typename T::iterator, typename U::iterator> end() const {
+    ConcatenatedIterator<typename T::const_iterator, typename U::const_iterator> end() const {
         return end_iterator;
     }
 
 private:
-    ConcatenatedIterator<typename T::iterator, typename U::iterator> begin_iterator, end_iterator;
+    ConcatenatedIterator<typename T::const_iterator, typename U::const_iterator> begin_iterator, end_iterator;
 };
 
 #endif //CONCATENATEDCONTAINERVIEW_HPP
